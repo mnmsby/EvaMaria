d4e243e3877d81f52f25f606471fb821a98a1f0c
mnmsby <91547882+mnmsby@users.noreply.github.com>
2022-04-05 16:58:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -256,7 +256,7 @@ async def help(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text=script.HELP_TEXT,
+        text=script.HELP_TXT,
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
